14b7b4b9bcb51afd9f7ad69f6bd1f1dd11c63b76
jacques jordaan <jordaanza@gmail.com>
2016-04-28 14:09:55 +0200

    placable class

---
+++ Assets/Scripts/Placable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Placable : MonoBehaviour
+{
+    public virtual void Awake()
+    {
+        
+    }
+
+    public virtual void Start()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public void Orientate(Vector3 normal)
+    {
+        
+    }
+}
