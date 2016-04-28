61c835f11cc2cbd77dcebacc0180a5f7dbe28f36
Chiggy\Chiggy <casey.lloyd.mgid@gmail.com>
2016-04-28 12:20:28 +0200

    getting there more or less
    getting there more or less

--- Assets/Scripts/ChiggyTemp/EditModeTest.cs
+++ Assets/Scripts/ChiggyTemp/EditModeTest.cs
@@ -4,33 +4,41 @@ using System.Collections;
 [ExecuteInEditMode]
 public class EditModeTest : MonoBehaviour
 {
-    public GameObject target;
-    private float targetRadius;
+    public float rotationSpeed = 120.0f;
+    public float translationSpeed = 10.0f;
+    public float height = 0.0f;             //height from ground level
+    private Transform centre;               //transform for planet
+    private float radius;                   //calculated radius from collider
+    public SphereCollider planet;           //collider for planet
 
     void Start()
     {
-        targetRadius = target.GetComponent<SphereCollider>().radius;
+        //consider scale applied to planet transform (assuming uniform, just pick one)
+        radius = planet.radius * planet.transform.localScale.y;
+        centre = planet.transform;
+        //starting position at north pole
+        transform.position = centre.position + new Vector3(0, radius + height, 0);
     }
 
-	void Update ()
+    void Update()
     {
-        if (target)
-        {
-            transform.LookAt(target.transform);
-            snapToGlobe();
-        }
-	}
-
-    private float squared(float value)
-    {
-        return value * value;
-    }
-
-    private void snapToGlobe()
-    {
-        if(squared(transform.position.x - target.transform.position.x) > targetRadius)
-        {
-            transform.position = new Vector3((target.transform.position.x + targetRadius), transform.position.y, transform.position.z);
-        }
+        //translate based on input     
+        float inputMag = Input.GetAxis("Vertical") * translationSpeed * Time.deltaTime;
+        transform.position += transform.forward * inputMag;
+        //snap position to radius + height (could also use raycasts)
+        Vector3 targetPosition = transform.position - centre.position;
+        float ratio = (radius + height) / targetPosition.magnitude;
+        targetPosition.Scale(new Vector3(ratio, ratio, ratio));
+        transform.position = targetPosition + centre.position;
+        //calculate planet surface normal                      
+        Vector3 surfaceNormal = transform.position - centre.position;
+        surfaceNormal.Normalize();
+        //GameObject's heading
+        float headingDeltaAngle = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        Quaternion headingDelta = Quaternion.AngleAxis(headingDeltaAngle, transform.up);
+        //align with surface normal
+        transform.rotation = Quaternion.FromToRotation(transform.up, surfaceNormal) * transform.rotation;
+        //apply heading rotation
+        transform.rotation = headingDelta * transform.rotation;
     }
 }
