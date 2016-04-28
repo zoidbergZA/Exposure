415b5b76b6d03f7686676abf00243e231ec334b2
Chiggy\Chiggy <casey.lloyd.mgid@gmail.com>
2016-04-28 11:16:40 +0200

    somewhat progress
    somewhat progress

---
+++ Assets/Scripts/ChiggyTemp/EditModeTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class EditModeTest : MonoBehaviour
+{
+    public GameObject target;
+    private float targetRadius;
+
+    void Start()
+    {
+        targetRadius = target.GetComponent<SphereCollider>().radius;
+    }
+
+	void Update ()
+    {
+        if (target)
+        {
+            transform.LookAt(target.transform);
+            snapToGlobe();
+        }
+	}
+
+    private float squared(float value)
+    {
+        return value * value;
+    }
+
+    private void snapToGlobe()
+    {
+        if(squared(transform.position.x - target.transform.position.x) > targetRadius)
+        {
+            transform.position = new Vector3((target.transform.position.x + targetRadius), transform.position.y, transform.position.z);
+        }
+    }
+}
