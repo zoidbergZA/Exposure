bf31f9b28a2d34d2e70ee1c6afd800d0767e43c6
Chiggy\Chiggy <casey.lloyd.mgid@gmail.com>
2016-05-09 12:47:49 +0200

    1 iteration
    1 iteration

--- Assets/Scripts/Placer.cs
+++ Assets/Scripts/Placer.cs
@@ -9,48 +9,58 @@ public class Placer : MonoBehaviour
     public float height = 0.5f;             //height from ground level
     private Transform centre;               //transform for planet
     private float radius;                   //calculated radius from collider
-    public SphereCollider planet;           //collider for planet
-    //public MeshCollider planet;           //collider for planet
+    //public SphereCollider planet;           //collider for planet
+    public Collider planet;           //collider for planet
+    private RaycastHit hit;
+    private Vector3 prevPos;
 
     void Start()
     {
-        transform.position = new Vector3(1, 1, 1);
         //consider scale applied to planet transform (assuming uniform, just pick one)
-        radius = planet.radius * planet.transform.localScale.y;
+        //radius = planet.radius * planet.transform.localScale.y;
         centre = planet.transform;
+        transform.position = planet.transform.position * 24;
+    }
+
+    private bool hitsTarget(float fromDistance)
+    {
+        if (Physics.Raycast(transform.position, -transform.up, out hit, fromDistance) || Physics.Raycast(transform.position, transform.up, out hit, fromDistance) ||
+            Physics.Raycast(transform.position, -transform.right, out hit, fromDistance) || Physics.Raycast(transform.position, transform.right, out hit, fromDistance) ||
+            Physics.Raycast(transform.position, -transform.forward, out hit, fromDistance) || Physics.Raycast(transform.position, transform.forward, out hit, fromDistance))
+        {
+            if (hit.collider.Equals(planet))
+            {
+                return true;
+            }
+            else return false;
+        }
+        else return false;
     }
 
     void Update()
     {
-        //translate based on input     
-        float inputMag = Input.GetAxis("Vertical") * translationSpeed * Time.deltaTime;
-        transform.position += transform.forward * inputMag;
         //snap position to radius + height (could also use raycasts)
-        Vector3 targetPosition = transform.position - centre.position;
-        float ratio = (radius + height) / targetPosition.magnitude;
-        targetPosition.Scale(new Vector3(ratio, ratio, ratio));
-        transform.position = targetPosition + centre.position;
-        //calculate planet surface normal                      
-        Vector3 surfaceNormal = transform.position - centre.position;
-        surfaceNormal.Normalize();
-        //GameObject's heading
-        float headingDeltaAngle = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
-        Quaternion headingDelta = Quaternion.AngleAxis(headingDeltaAngle, transform.up);
-        //align with surface normal
-        transform.rotation = Quaternion.FromToRotation(transform.up, surfaceNormal) * transform.rotation;
-        //apply heading rotation
-        transform.rotation = headingDelta * transform.rotation;
+        //Vector3 targetPosition = transform.position - centre.position;
+        //float ratio = (radius + height) / targetPosition.magnitude;
+        //targetPosition.Scale(new Vector3(ratio, ratio, ratio));
+        //transform.position = targetPosition + centre.position;
+        ////calculate planet surface normal                      
+        //Vector3 surfaceNormal = transform.position - centre.position;
+        //surfaceNormal.Normalize();
+        ////align with surface normal
+        //transform.rotation = Quaternion.FromToRotation(transform.up, surfaceNormal) * transform.rotation;
 
+        if (hitsTarget(100))
+        {
+            // Stick on surface
+            if((hit.point - centre.position).magnitude <= (transform.position - centre.position).magnitude)
+            {
+                transform.position = hit.point + hit.normal * height;
+            }
 
-        //RaycastHit hit;
-
-        //if (Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity))
-        //{
-        //    // Stick on surface
-        //    transform.position = hit.point + hit.normal * height;
-
-        //    // Align to surface normal
-        //    transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-        //}
+            // Align to surface normal
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+        //else transform.position = prevPos;
     }
 }
