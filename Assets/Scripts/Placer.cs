f8cc12f248eb58578b9d32e245c1d2697bf7805f
Chiggy\Chiggy <casey.lloyd.mgid@gmail.com>
2016-05-10 11:33:56 +0200

    3 iteration
    3 iteration

--- Assets/Scripts/Placer.cs
+++ Assets/Scripts/Placer.cs
@@ -4,34 +4,20 @@ using System.Collections;
 [ExecuteInEditMode]
 public class Placer : MonoBehaviour
 {
-    public float rotationSpeed = 120.0f;
-    public float translationSpeed = 10.0f;
-    public float height = 0.5f;             //height from ground level
-    private Transform centre;               //transform for planet
-    private float radius;                   //calculated radius from collider
-    //public SphereCollider planet;           //collider for planet
-    public Collider planet;           //collider for planet
+    public float height = 0.5f;     //height from ground level
+    public Collider planet;         //collider for planet
     private RaycastHit hit;
-    private Vector3 prevPos;
 
-    void Start()
+    private Vector3 getDirection()
     {
-        //consider scale applied to planet transform (assuming uniform, just pick one)
-        //radius = planet.radius * planet.transform.localScale.y;
-        centre = planet.transform;
-        transform.position = planet.transform.position * 24;
+        return (planet.transform.position - transform.position).normalized;
     }
 
     private bool hitsTarget(float fromDistance)
     {
-        if (Physics.Raycast(transform.position, -transform.up, out hit, fromDistance) || Physics.Raycast(transform.position, transform.up, out hit, fromDistance) ||
-            Physics.Raycast(transform.position, -transform.right, out hit, fromDistance) || Physics.Raycast(transform.position, transform.right, out hit, fromDistance) ||
-            Physics.Raycast(transform.position, -transform.forward, out hit, fromDistance) || Physics.Raycast(transform.position, transform.forward, out hit, fromDistance))
+        if (Physics.Raycast(transform.position, getDirection(), out hit, fromDistance))
         {
-            if (hit.collider.Equals(planet))
-            {
-                return true;
-            }
+            if (hit.collider.Equals(planet)) return true;
             else return false;
         }
         else return false;
@@ -39,28 +25,10 @@ public class Placer : MonoBehaviour
 
     void Update()
     {
-        //snap position to radius + height (could also use raycasts)
-        //Vector3 targetPosition = transform.position - centre.position;
-        //float ratio = (radius + height) / targetPosition.magnitude;
-        //targetPosition.Scale(new Vector3(ratio, ratio, ratio));
-        //transform.position = targetPosition + centre.position;
-        ////calculate planet surface normal                      
-        //Vector3 surfaceNormal = transform.position - centre.position;
-        //surfaceNormal.Normalize();
-        ////align with surface normal
-        //transform.rotation = Quaternion.FromToRotation(transform.up, surfaceNormal) * transform.rotation;
-
         if (hitsTarget(100))
         {
-            // Stick on surface
-            if((hit.point - centre.position).magnitude <= (transform.position - centre.position).magnitude)
-            {
-                transform.position = hit.point + hit.normal * height;
-            }
-
-            // Align to surface normal
-            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            transform.position = hit.point + hit.normal * height; // Stick on surface
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation; // Align to surface normal
         }
-        //else transform.position = prevPos;
     }
 }
