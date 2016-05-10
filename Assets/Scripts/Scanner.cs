d3a22cdbf4e6b0c7d1ecab6bfdc248527d74dabf
Chiggy\Chiggy <casey.lloyd.mgid@gmail.com>
2016-05-10 12:25:56 +0200

    1 iteration
    1 iteration

--- Assets/Scripts/Scanner.cs
+++ Assets/Scripts/Scanner.cs
@@ -5,6 +5,9 @@ public class Scanner : MonoBehaviour
 {
     [SerializeField] private float cooldownTime = 7f;
     private float cooldown;
+    public GameObject target;
+    private Material material;
+    private Renderer renderer;
 
     public bool IsReady { get; private set; }
     public float Cooldown { get { return cooldown; } }
@@ -14,15 +17,23 @@ public class Scanner : MonoBehaviour
         IsReady = true;
     }
 
+    void Start()
+    {
+        renderer = target.GetComponent<Renderer>();
+        material = renderer.material;
+    }
+
     void Update()
     {
         if (!IsReady)
         {
             cooldown -= Time.deltaTime;
 
+            material.SetFloat("_ScanFactor", Mathf.Sin(Time.time));
             if (cooldown <= 0)
             {
                 IsReady = true;
+                material.SetFloat("_ScanFactor", 1);
             }
         }
     }
@@ -31,7 +42,5 @@ public class Scanner : MonoBehaviour
     {
         IsReady = false;
         cooldown = cooldownTime;
-
-        // todo: Add scanner shader logic here
     }
 }
