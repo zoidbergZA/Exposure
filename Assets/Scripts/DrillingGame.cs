46f46d916b943cf50dc07435585442829fc6de9b
Chiggy\Chiggy <casey.lloyd.mgid@gmail.com>
2016-05-12 23:56:33 +0200

    1 iteration
    1 iteration

--- Assets/Scripts/DrillingGame.cs
+++ Assets/Scripts/DrillingGame.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class DrillingGame : Minigame
 {
     [SerializeField] private GeoThermalPlant geoThermalPlantPrefab;
+    [SerializeField] private UnityEngine.UI.Image bgActive;
+    [SerializeField] private UnityEngine.UI.Image bgInactive;
+    [SerializeField] private UnityEngine.UI.Image drill;
     private Drillspot drillspot;
 
     public void StartGame(Drillspot drillspot)
@@ -14,6 +17,17 @@ public class DrillingGame : Minigame
         this.drillspot = drillspot;
 
         Begin();
+
+        if (bgActive) bgActive.gameObject.SetActive(true);
+        if (bgInactive) bgInactive.gameObject.SetActive(false);
+        if (drill) drill.gameObject.SetActive(true);
+    }
+
+    void Start()
+    {
+        if (bgActive) bgActive.gameObject.SetActive(false);
+        if (bgInactive) bgInactive.gameObject.SetActive(true);
+        if (drill) drill.gameObject.SetActive(false);
     }
 
     public override void Update()
@@ -23,6 +37,8 @@ public class DrillingGame : Minigame
         //todo: minigame logic here, auto-win close to timeOut for now
         if (IsRunning && Timeleft <= 0.5f)
             End(true);
+
+        if (drill) drill.gameObject.transform.Translate(new Vector3(Mathf.Sin(Time.time)*2, 0, 0));
     }
 
     public override void End(bool succeeded)
