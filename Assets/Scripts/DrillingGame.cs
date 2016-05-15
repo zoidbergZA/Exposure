adc0883f90cd618a6d646c33d4ee9bc31b6959aa
Chiggy\Chiggy <casey.lloyd.mgid@gmail.com>
2016-05-15 17:34:32 +0200

    9 iteration
    9 iteration

--- Assets/Scripts/DrillingGame.cs
+++ Assets/Scripts/DrillingGame.cs
@@ -24,6 +24,7 @@ public class DrillingGame : Minigame
     private int targetRow;
     private bool slidingLeft = false;
     private List<GameObject> rocks = new List<GameObject>();
+    private GameObject[,] objTable = new GameObject[5,10];
     public DrillingGameState GetState { get { return state; } }
     public void SetMakeDrill(bool value) { makeDrill = value; }
     public UnityEngine.UI.Image GetDrill { get { return drill; } }
@@ -61,6 +62,7 @@ public class DrillingGame : Minigame
                         rock.GetComponent<RectTransform>().anchoredPosition = new Vector3(columns[i], rows[j]);
                         rock.gameObject.SetActive(true);
                         rocks.Add(rock);
+                        objTable[i, j] = rock;
                     }
                     else
                     {
@@ -69,6 +71,7 @@ public class DrillingGame : Minigame
                         diamond.GetComponent<RectTransform>().anchoredPosition = new Vector3(columns[i], rows[j]);
                         diamond.gameObject.SetActive(true);
                         rocks.Add(diamond);
+                        objTable[i, j] = diamond;
                     }
                 }
             }
@@ -167,7 +170,6 @@ public class DrillingGame : Minigame
                 {
                     state = DrillingGameState.DRILLING;
                     if (targetColumn < columns.Length - 1) targetColumn += 1;
-                    targetRow = 0;
                 }
             }
             else
@@ -177,7 +179,6 @@ public class DrillingGame : Minigame
                 {
                     state = DrillingGameState.DRILLING;
                     if (targetColumn > 0) targetColumn -= 1;
-                    targetRow = 0;
                 }
             }
         }
@@ -205,6 +206,24 @@ public class DrillingGame : Minigame
             End(false);
             slidingLeft = false;
         }
+        updateCollisions();
+    }
+
+    private void updateCollisions()
+    {
+        if (objTable[targetColumn, targetRow] != null)
+        {
+            if (objTable[targetColumn, targetRow].tag == "Rock")
+            {
+                End(false);
+                state = DrillingGameState.INACTIVE;
+            }
+            else if (objTable[targetColumn, targetRow].tag == "Diamond")
+            {
+                Destroy(objTable[targetColumn, targetRow]);
+                GameManager.Instance.Player.ScorePoints(10);
+            }
+        }
     }
 
     public override void End(bool succeeded)
@@ -215,21 +234,11 @@ public class DrillingGame : Minigame
         {
             GeoThermalPlant plant = Instantiate(geoThermalPlantPrefab, drillspot.transform.position, drillspot.transform.rotation) as GeoThermalPlant;
             Destroy(drillspot.gameObject);
-            
-            makeDrill = false;
-            slidingLeft = false;
             GameManager.Instance.Player.StartBuildMinigame(plant);
         }
-        else
-        {
-            GameManager.Instance.Player.GoToNormalState(GameManager.Instance.PlanetTransform);
-            makeDrill = false;
-            slidingLeft = false;
-        }
-        foreach(GameObject rock in rocks)
-        {
-            Destroy(rock);
-        }
+        else GameManager.Instance.Player.GoToNormalState(GameManager.Instance.PlanetTransform);
+        
+        resetGameGuts();
     }
 
     private void activateImages(bool activate)
@@ -244,7 +253,18 @@ public class DrillingGame : Minigame
             if (bgActive) bgActive.gameObject.SetActive(false);
             if (bgInactive) bgInactive.gameObject.SetActive(true);
             if (drill) drill.gameObject.SetActive(false);
-            targetColumn = 0;
         }
+    }
+
+    private void resetGameGuts()
+    {
+        makeDrill = false;
+        slidingLeft = false;
+        targetColumn = 0;
+
+        foreach (GameObject rock in rocks) Destroy(rock);
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 10; j++)
+                objTable[i, j] = null;
     }
 }
