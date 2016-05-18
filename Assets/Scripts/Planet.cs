1615f38c7bf8ac57c4bf80a3394d55da0345711a
jacques jordaan <jordaanza@gmail.com>
2016-05-18 11:36:24 +0200

    planet prefab made easier to modify

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -1,15 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Planet : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class Planet : MonoBehaviour
+{
+    public MeshRenderer scannableMesh; // expand to array later?
 }
