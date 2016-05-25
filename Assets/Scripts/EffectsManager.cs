e91344e432c323579a21d5f81a367acf69625c02
jacques jordaan <jordaanza@gmail.com>
2016-05-25 12:29:12 +0200

    special effects

--- Assets/Scripts/EffectsManager.cs
+++ Assets/Scripts/EffectsManager.cs
@@ -3,5 +3,54 @@ using System.Collections;
 
 public class EffectsManager : MonoBehaviour
 {
-    
+    //prefabs
+    [SerializeField] private int bufferSize = 10;
+    [SerializeField] private SpecialEffect[] effectPrefabs;
+
+    private RotatingBuffer[] buffers;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    public void Play(int effectPrefab, Vector3 position, Quaternion rotation)
+    {
+        buffers[effectPrefab].Play(position, rotation);
+    }
+
+    private void Initialize()
+    {
+        buffers = new RotatingBuffer[effectPrefabs.Length];
+
+        for (int i = 0; i < buffers.Length; i++)
+        {
+            buffers[i] = new RotatingBuffer(bufferSize);
+
+            for (int j = 0; j < buffers[i].effects.Length; j++)
+            {
+                buffers[i].effects[j] = Instantiate(effectPrefabs[i], Vector3.zero, Quaternion.identity) as SpecialEffect;
+                buffers[i].effects[j].transform.SetParent(transform);
+                buffers[i].effects[j].Reset();
+            }
+        }
+    }
+}
+
+public class RotatingBuffer
+{
+    public SpecialEffect[] effects;
+    private int head;
+
+    public RotatingBuffer(int bufferSize)
+    {
+        effects = new SpecialEffect[bufferSize];
+    }
+
+    public void Play(Vector3 position, Quaternion rotation)
+    {
+        //play effect at head
+        
+        //move head        
+    }
 }
