1229128a6359f542ecb6c2a68eadd4edad560153
jacques jordaan <jordaanza@gmail.com>
2016-05-30 22:22:45 +0200

    orbiters

--- Assets/Scripts/Orbiter.cs
+++ Assets/Scripts/Orbiter.cs
@@ -6,14 +6,23 @@ public class Orbiter : MonoBehaviour
     [SerializeField] private Transform model;
     [SerializeField] private float speed = 2f;
 
+    private Vector3 axis;
+    private Vector3 oldPosition;
+
 	void Start ()
-    {
-	
+	{
+	    oldPosition = transform.position;
+	    axis = Random.insideUnitSphere;
 	}
 
     void Update ()
     {
-	    transform.RotateAround(GameManager.Instance.Planet.transform.position, Vector3.up, speed * Time.deltaTime);
-        transform.LookAt(GameManager.Instance.Planet.transform);
-	}
+	    transform.RotateAround(GameManager.Instance.Planet.transform.position, axis, speed * Time.deltaTime);
+
+        Vector3 forward = transform.position + (transform.position - oldPosition) * 50f;
+ 
+        model.LookAt(forward, transform.position - GameManager.Instance.PlanetTransform.position);
+
+        oldPosition = transform.position;
+    }
 }
