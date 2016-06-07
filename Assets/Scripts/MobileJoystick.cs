2516f51caacdbdfc1846cac66a15ce1784fa2676
jacques jordaan <jordaanza@gmail.com>
2016-06-07 10:44:39 +0200

    touch input

--- Assets/Scripts/MobileJoystick.cs
+++ Assets/Scripts/MobileJoystick.cs
@@ -6,10 +6,14 @@ using UnityStandardAssets.CrossPlatformInput;
 public class MobileJoystick : MonoBehaviour
 {
     [SerializeField] private GameObject joystickPanel;
+    [SerializeField] private float minDragDistance = 20f;
 
     public DrillingDirection CurrentInput { get; private set; }
     public GameObject JoystickPanel { get { return joystickPanel; } }
     public Vector2 JoystickInput { get; private set; }
+
+    private Vector2 dragPrevious;
+    private Vector2 dragStart;
 
     void Awake()
     {
@@ -27,14 +31,54 @@ public class MobileJoystick : MonoBehaviour
     {
         Vector2 input = Vector2.zero;
 
-        if (Input.GetKey(KeyCode.UpArrow))
-            input.y = 1f;
-        else if (Input.GetKey(KeyCode.DownArrow))
-            input.y = -1f;
-        else if (Input.GetKey(KeyCode.LeftArrow))
-            input.x = -1f;
-        else if (Input.GetKey(KeyCode.RightArrow))
-            input.x = 1f;
+        if (GameManager.Instance.TouchInput)
+        {
+            if (Input.touchCount > 0)
+            {
+                if (Input.touches[0].phase == TouchPhase.Began)
+                    dragStart = Input.touches[0].position;
+
+                Vector2 dragDelta = Input.touches[0].position - dragStart;
+
+                if (dragDelta.sqrMagnitude >= minDragDistance)
+                {
+                    input = Input.touches[0].deltaPosition;
+                }
+
+//                dragPrevious = Input.mousePosition;
+            }
+        }
+        else
+        {
+            //try keyboard input first
+            if (Input.GetKey(KeyCode.UpArrow))
+                input.y = 1f;
+            else if (Input.GetKey(KeyCode.DownArrow))
+                input.y = -1f;
+            else if (Input.GetKey(KeyCode.LeftArrow))
+                input.x = -1f;
+            else if (Input.GetKey(KeyCode.RightArrow))
+                input.x = 1f;
+
+            //then try mouse input
+            if (input.sqrMagnitude < 0.1f)
+            {
+                if (Input.GetMouseButtonDown(0))
+                    dragStart = Input.mousePosition;
+
+                if (Input.GetMouseButton(0))
+                {
+                    Vector2 dragDelta = (Vector2)Input.mousePosition - dragStart;
+
+                    if (dragDelta.sqrMagnitude >= minDragDistance)
+                    {
+                        input = (Vector2) Input.mousePosition - dragPrevious;
+                    }
+
+                    dragPrevious = Input.mousePosition;
+                }
+            }
+        }
 
         JoystickInput = input;
         SetCurrentDirection();
