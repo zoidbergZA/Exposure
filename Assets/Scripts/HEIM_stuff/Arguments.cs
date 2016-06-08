7098769c23b64d6f268bf3b62db45ea790d1b8a7
Chiggy\Chiggy <casey.lloyd.mgid@gmail.com>
2016-06-08 08:14:27 +0200

    heim stuff
    heim stuff

--- Assets/Scripts/HEIM_stuff/Arguments.cs
+++ Assets/Scripts/HEIM_stuff/Arguments.cs
@@ -2,47 +2,46 @@ using UnityEngine;
 using System.Collections;
 using System;
 
-public class Arguments : MonoBehaviour {
-	
+public class Arguments : MonoBehaviour
+{
 	private int userID;
 	private int gameID;
 	private string username;
 	private int gametime;
 	private string conURL;
 	
-	void Awake() {
+	void Awake()
+    {
 		string[] arguments = Environment.GetCommandLineArgs();
 		userID = Convert.ToInt32(arguments[2]);
 		gameID = Convert.ToInt32(arguments[3]);
 		username = arguments[4];
 		gametime = Convert.ToInt32(arguments[5]);
 		conURL = arguments[6];
-//
-//		userID = 2;
-//		gameID = 6;
-//		username = "YVONNE";
-//		gametime = 10;
-//		conURL = "http://www.serellyn.net/HEIM/php/";
-
 	}
 	
-	public int getUserID() {
+	public int getUserID()
+    {
 		return userID;	
 	}
 	
-	public int getGameID() {
+	public int getGameID()
+    {
 		return gameID;	
 	}
 	
-	public string getUsername() {
+	public string getUsername() 
+    {
 		return username;
 	}
 	
-	public int getGameTime() {
+	public int getGameTime()
+    {
 		return gametime;
 	}
 
-	public string getConURL() {
+	public string getConURL()
+    {
 		return conURL;
 	}
 }
--- Assets/Scripts/HEIM_stuff/DBconnection.cs
+++ Assets/Scripts/HEIM_stuff/DBconnection.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 using System.Collections;
 
-public class DBconnection : MonoBehaviour {
+public class DBconnection : MonoBehaviour
+{
 	private Arguments argumentsScript;
 	private string connectionURL;
 	private string scoreURL = "insertScore.php?";
 
-	void Awake() {
-		argumentsScript = GameObject.Find("SceneManager").GetComponent<Arguments>();
+	void Awake()
+    {
+        argumentsScript = FindObjectOfType<Arguments>();
+		//argumentsScript = GameObject.Find("SceneManager").GetComponent<Arguments>();
 	}
-	void Start() {
+	void Start()
+    {
 		connectionURL = argumentsScript.getConURL();
 	}
 
-	public IEnumerator UploadScore(int userID, int gameID, int score) {
+	public IEnumerator UploadScore(int userID, int gameID, int score)
+    {
 		string full_url = connectionURL + scoreURL + "userID=" + userID + "&gameID=" + gameID + "&score=" + score;
 		WWW post = new WWW(full_url);
 		yield return post;
