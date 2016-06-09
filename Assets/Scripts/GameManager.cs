dee4456f9816ddc7101474c43fa48080ac483240
jacques jordaan <jordaanza@gmail.com>
2016-06-09 11:37:03 +0200

    game auto start bool added.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,6 +29,7 @@ public class GameManager : MonoBehaviour
     public GameObject PipePrefab;
     public Tutorial TutorialPrefab;
 
+    public bool autoStart;
     public bool enableTutorial;
     public bool showDebug;
     public TextAsset puzzle1;
@@ -94,6 +95,9 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         int[] puzzle = LoadDrillingPuzzle(puzzle1);
+
+        if (autoStart)
+            Hud.OnStartRoundClicked();
     }
 
     void Update()
