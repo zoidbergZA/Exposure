ef9e141ca5b0d83a05811fe60b14744a765606d4
jacques jordaan <jordaanza@gmail.com>
2016-06-21 12:42:09 +0200

    volcano

---
+++ Assets/Scripts/Volcano.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Volcano : MonoBehaviour
+{
+    [SerializeField] private float cooldownMin = 5f;
+    [SerializeField] private float cooldownMax = 15f;
+    [SerializeField] private ParticleSystem[] particleSystems;
+
+    void Start()
+    {
+        StartCoroutine(InitErruption());
+    }
+
+    private IEnumerator InitErruption()
+    {
+        yield return new WaitForSeconds(Random.Range(cooldownMin, cooldownMax));
+
+        for (int i = 0; i < particleSystems.Length; i++)
+        {
+            particleSystems[i].Play();
+        }
+    }
+}
